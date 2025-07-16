e8fcfcf8cf8c561845ec20a542d2d0f2ca8208bf
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2025-07-16 12:10:30 +0100

    github images

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        // domains: [
-        //     'raw.githubusercontent.com'
-        // ],
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'raw.githubusercontent.com',
+                port: '',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'github.com',
                 port: '',
                 pathname: '**',
             },
